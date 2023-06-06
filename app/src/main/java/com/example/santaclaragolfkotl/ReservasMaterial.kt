3076bd13429342481b8d8537734a4e61799d8486
daniidoblass14 <105448332+daniidoblass14@users.noreply.github.com>
2023-06-06 18:25:29 +0200

    commit 9

--- app/src/main/java/com/example/santaclaragolfkotl/ReservasMaterial.kt
+++ app/src/main/java/com/example/santaclaragolfkotl/ReservasMaterial.kt
@@ -3,10 +3,15 @@ package com.example.santaclaragolfkotl
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -25,11 +30,52 @@ class ReservasMaterial: AppCompatActivity() {
     private var guantesDatos = mutableListOf<String>()
     private var arreglaPiquesDatos = mutableListOf<String>()
 
+    private var btnContinue: Button? = null
+    private var btnConfirm: Button? = null
+    private var btnCancel: Button? = null
+
+    private var cardView: CardView? = null
+    private var username: String? = null
+    private var userPhone: String? = null
+
+    private var textViewName: TextView? = null
+    private var textViewPhone: TextView? = null
+    private var textViewSticks: TextView? = null
+    private var textViewGloves: TextView? = null
+    private var textViewShoes: TextView? = null
+    private var textViewBalls: TextView? = null
+    private var textViewPitchFork: TextView? = null
+
+    private var textInputLayoutPalos: TextInputLayout? = null
+    private var textInputLayoutGuantes: TextInputLayout? = null
+    private var textInputLayoutPelotas: TextInputLayout? = null
+    private var textInputLayoutCalzados: TextInputLayout? = null
+    private var textInputLayoutArreglapiques: TextInputLayout? = null
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.material_reservation_layout)
+
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+
+        textInputLayoutPalos = findViewById<TextInputLayout>(R.id.textInputLayoutPalos)
+        textInputLayoutGuantes = findViewById<TextInputLayout>(R.id.textInputLayoutGuantes)
+        textInputLayoutPelotas = findViewById<TextInputLayout>(R.id.textInputLayoutPelotas)
+        textInputLayoutCalzados = findViewById<TextInputLayout>(R.id.textInputLayoutCalzados)
+        textInputLayoutArreglapiques = findViewById<TextInputLayout>(R.id.textInputLayoutArreglapiques)
+
+
+
+        textViewName = findViewById<TextView>(R.id.textViewName)
+        textViewPhone = findViewById<TextView>(R.id.textViewPhone)
+        textViewSticks = findViewById<TextView>(R.id.textViewSticks)
+        textViewGloves = findViewById<TextView>(R.id.textViewGloves)
+        textViewShoes = findViewById<TextView>(R.id.textViewShoes)
+        textViewBalls = findViewById<TextView>(R.id.textViewBalls)
+        textViewPitchFork = findViewById<TextView>(R.id.textViewPitchFork)
+
 
         palosDropdown = findViewById<AutoCompleteTextView>(R.id.palosFilledExposedDropdown)
         guantesDropdown = findViewById<AutoCompleteTextView>(R.id.guantesFilledExposedDropdown)
@@ -37,7 +83,93 @@ class ReservasMaterial: AppCompatActivity() {
         calzadoDropdown = findViewById<AutoCompleteTextView>(R.id.calzadoFilledExposedDropdown)
         arreglaPiquesDropdown = findViewById<AutoCompleteTextView>(R.id.arreglaPiquesFilledExposedDropdown)
 
+        cardView = findViewById<CardView>(R.id.cardView)
+
+        btnContinue = findViewById<Button>(R.id.btnContinue)
+        btnConfirm = findViewById<Button>(R.id.btnConfirm)
+        btnCancel = findViewById<Button>(R.id.btnCancel)
+
         rellenar()
+
+        btnContinue?.setOnClickListener {
+
+            btnContinue?.visibility = View.GONE
+            textInputLayoutPalos?.visibility = View.GONE
+            textInputLayoutGuantes?.visibility = View.GONE
+            textInputLayoutPelotas?.visibility = View.GONE
+            textInputLayoutCalzados?.visibility = View.GONE
+            textInputLayoutArreglapiques?.visibility = View.GONE
+
+            cardView?.visibility = View.VISIBLE
+            btnConfirm?.visibility = View.VISIBLE
+            btnCancel?.visibility = View.VISIBLE
+
+            palosDropdown?.isEnabled = false
+            guantesDropdown?.isEnabled = false
+            pelotasDropdown?.isEnabled = false
+            calzadoDropdown?.isEnabled = false
+            arreglaPiquesDropdown?.isEnabled = false
+
+            val layoutParams = cardView?.layoutParams as ConstraintLayout.LayoutParams
+            layoutParams.topToBottom = R.id.textInputLayoutGuests
+            cardView?.layoutParams = layoutParams
+
+            if (currentUser != null && currentUser.email != null) {
+                val userEmail = currentUser.email
+
+                db.collection("users").whereEqualTo("email", userEmail.toString()).get()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val querySnapshot = task.result
+                            if (querySnapshot != null && !querySnapshot.isEmpty) {
+                                val document = querySnapshot.documents[0]
+                                username = document.getString("nombre")
+                                userPhone = document.getString("telefono")
+
+                                // Utiliza los valores obtenidos del documento del usuario
+                                textViewName?.text = username
+                                textViewPhone?.text = userPhone
+                            } else {
+                                // No se encontró ningún documento con el email del usuario actual
+                            }
+                        } else {
+                            // Error al realizar la consulta
+                        }
+                    }
+            }
+            textViewSticks?.text = palosDropdown?.text.toString()
+            textViewGloves?.text = guantesDropdown?.text.toString()
+            textViewShoes?.text = calzadoDropdown?.text.toString()
+            textViewBalls?.text = pelotasDropdown?.text.toString()
+            textViewPitchFork?.text = arreglaPiquesDropdown?.text.toString()
+        }
+
+        btnCancel?.setOnClickListener {
+            btnContinue?.visibility = View.VISIBLE
+            textInputLayoutPalos?.visibility = View.VISIBLE
+            textInputLayoutGuantes?.visibility = View.VISIBLE
+            textInputLayoutPelotas?.visibility = View.VISIBLE
+            textInputLayoutCalzados?.visibility = View.VISIBLE
+            textInputLayoutArreglapiques?.visibility = View.VISIBLE
+
+            cardView?.visibility = View.GONE
+            btnConfirm?.visibility = View.GONE
+            btnCancel?.visibility = View.GONE
+
+            palosDropdown?.isEnabled = true
+            guantesDropdown?.isEnabled = true
+            calzadoDropdown?.isEnabled = true
+            pelotasDropdown?.isEnabled = true
+            arreglaPiquesDropdown?.isEnabled = true
+
+            palosDropdown?.setText("")
+            guantesDropdown?.setText("")
+            calzadoDropdown?.setText("")
+            pelotasDropdown?.setText("")
+            arreglaPiquesDropdown?.setText("")
+
+        }
+
     }
 
     private fun rellenar() {
